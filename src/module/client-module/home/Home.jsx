96f6c9641eff507e4266d6f893d3802d8ad1e585
Ha <pnha03dev.it@gmail.com>
2025-02-08 14:00:00 +0100

    client

---
+++ src/module/client-module/home/Home.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import "./home.scss";
+import { Container, Row, Col } from "react-bootstrap"; // Thêm Row và Col để tạo cấu trúc lưới
+import { Box } from "@mui/material";
+import CategoryNav from "./CategoryNav";
+import ProductList from "./ProductList";
+import { useQuery } from "@tanstack/react-query";
+import { getHomeProduct } from "../../../api/homeApi";
+import Loading from "../loading/Loading";
+import CHUSInfo from "./About";
+import BrandShowcase from "./Branch";
+
+const Home = () => {
+
+  const {data,isLoading} = useQuery({
+    queryKey:['product-home'],
+    queryFn: getHomeProduct
+  })
+  
+  if(isLoading){
+    return <Loading></Loading>
+  }
+  return (
+    <div id="home-page" style={{ paddingBottom: "200px" }}>
+      <CategoryNav />
+      <Container style={{ width: "80%" }}>
+        <Box sx={{ mt: 5 }}>
+          <Carousel style={{ width: "100%" }} interval={3000}>
+            <Carousel.Item>
+              <img
+                style={{ width: "100%", height: "420px", objectFit: "cover" }}
+                src="https://static.chus.vn/images/promo/279/Pc_CHUSCHOOSE_Vie.jpg"
+                alt=""
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                style={{ width: "100%", height: "420px", objectFit: "cover" }}
+                src="https://static.chus.vn/images/promo/283/Pc_TeacherDayGift_Vi.jpg"
+                alt=""
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                style={{ width: "100%", height: "420px", objectFit: "cover" }}
+                src="https://static.chus.vn/images/promo/283/Pc_BagCollection_Vie.jpg"
+                alt=""
+              />
+            </Carousel.Item>
+          </Carousel>
+        </Box>
+        {data.map((item) => {
+            return <ProductList products={item.products} category={item.name} id={item.id}></ProductList>
+        })}
+        
+        <CHUSInfo></CHUSInfo>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
