1f6ea7f577a33979b5ee08c16ee528ba8cdd91c3
Thuy-Linh2403 <dtlyinh.work@gmail.com>
2025-04-05 23:51:32 +0700

    Fix address api

--- src/api/addressApi.js
+++ src/api/addressApi.js
@@ -5,3 +5,16 @@ export const addAddress =async (content) => {
     return data.data
 }
 
+export const getAllAddress = async () =>{
+        let data = await fetch.get("/address/get-list")
+        return data.data.data
+}
+
+export const deleteAddress = async (id) => {
+    let data = await fetch.delete(`/address/delete/${id}`)
+    return data.data
+}
+export const updateAddress = async (content) => {
+    let data = await fetch.put("/address/update", content)
+    return data.data
+}
