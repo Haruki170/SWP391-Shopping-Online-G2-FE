45ef207c57dfa8edf485a0f5536410ac5f6a1b61
Haruki170 <181960990+Haruki170@users.noreply.github.com>
2025-04-07 23:38:49 +0700

    Add giao dien quan ly don tra hang

---
+++ src/module/admin-module/returnOrder/ReturnOrder.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ReturnOrderHeader.scss";
+
+const ReturnOrderHeader = () => {
+  return (
+    <div className="return-order-header">
+      <h1>Quản lý đơn hoàn trả</h1>
+    </div>
+  );
+};
